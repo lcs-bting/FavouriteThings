4612b598209c711ce08d4c75b03d16bf99c7ea11
BIll Ting <bting@lcs.on.ca>
2020-09-25 10:57:17 -0400

    Navigation #2 done

--- FavouriteThings/ContentView.swift
+++ FavouriteThings/ContentView.swift
@@ -14,67 +14,49 @@ struct ContentView: View {
     // that conforms to the view protocol must return
     // ONe and only ONE view
     var body: some View {
-         
+        
         //ONE VIEW AT TH TOP
-        ScrollView{
-            VStack {
-                Group {
+        NavigationView{
+            
+            List{
+                
+                NavigationLink(destination: AboutMeView()) {
                     
-                    Text("Hello, I'm Bill Ting!")
-                        .font(.largeTitle)
-                        .frame(width: 300.0, height: 150.0)
-                        .background(Color.purple)
-                    
-                    Image("Me")
-                        .resizable()
-                        .padding(.horizontal)
-                        .scaledToFit()
-                        .frame(width: 330.0)
-                    
-                    Text("Hi my name is Bill, I am from Taiwan. I like to play basketball and I have a dog. \n\n\n")
-                        .frame(width: 330.0)
-                    
-                    Text("My Pet")
-                        .font(.largeTitle)
-                        .frame(width: 120.0, height: 50.0)
-                        .background(Color.yellow)
-                    
-                    Image("Dog")
-                        .resizable()
-                        .padding(.horizontal)
-                        .scaledToFit()
-                        .frame(width: 330.0)
-                    
-                    Text("This is my dog, her name is CoCo. She is 10 years old and I like her so much. \n\n\n")
-                        .frame(width: 330.0)
-                    
-                    Text("My Country")
-                        .font(.largeTitle)
-                        .frame(width: 190.0, height: 50.0)
-                        .background(Color.yellow)
-                    
-                    Image("Taiwan")
-                        .resizable()
-                        .padding(.horizontal)
-                        .scaledToFit()
-                        .frame(width: 330.0)
-                    
-                    Text(" Taiwan, officially the Republic of China, is a country in East Asia. Neighbouring countries include the People's Republic of China to the northwest, Japan to the northeast, and the Philippines to the south. \n\n")
-                        .frame(width: 330.0)
+                    HStack {
+                        
+                        Image("Me")
+                            .resizable()
+                            .frame(width:50.0, height:50.0)
+                        
+                        //Provide the lable for the navigation link
+                        
+                        Text("About me")
+                    }
                     
                 }
-                
-                
-            }
+                NavigationLink(destination: ThingsView()) {
+                    
+                    HStack {
+                        
+                        Image("JR-500")
+                            .resizable()
+                            .frame(width:50.0, height:50.0)
+                        
+                        //Provide the lable for the navigation link
+                        
+                        Text("Things I like")
+                    }
+                }
         }
+
         
     }
     
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
+}
---
+++ FavouriteThings/ThingsView.swift
@@ -0,0 +1,50 @@
+//
+//  ThingsILikeView.swift
+//  FavouriteThings
+//
+//  Created by Bill Ting on 2020-09-23.
+//  Copyright © 2020 Bill Ting. All rights reserved.
+//
+
+import SwiftUI
+
+struct ThingsView: View {
+    var body: some View {
+        ScrollView{
+            VStack{
+                Text("Things I like")
+                    .font(.largeTitle)
+                    .frame(width: 300.0, height: 150.0)
+                    .background(Color.purple)
+            
+                Image("JR-500")
+                    .resizable()
+                    .padding(.horizontal)
+                    .scaledToFit()
+                    .frame(width: 330.0)
+                
+                Text("I like transportatations so much expecially high-speed-rail because it is the fastest transportation on land. \n\n ")
+                    .frame(width: 330.0)
+               
+                Image("F-16")
+                    .resizable()
+                    .padding(.horizontal)
+                    .scaledToFit()
+                    .frame(width: 330.0)
+                
+                Text("Military, the shield of the countery, I love and respect every single person who contributes themselves. \n\n ")
+                    .frame(width: 330.0)
+                
+                
+            }
+            
+        }
+        
+    }
+}
+
+struct ThingsILikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThingsView()
+    }
+}
